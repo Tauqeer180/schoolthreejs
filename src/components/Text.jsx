22372009533b21c73452f865b5d8e9339aed73fa
uswanadeem09675 <wtsplayer001@gamil.com>
2023-10-25 16:09:30 +0500

    added new grass

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -5,20 +5,18 @@ import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
 import fontsFile from "../font/Lato_Regular.json";
 const Text = () => {
   const textGroup = new THREE.Group();
-  console.log(fontsFile);
-  const loader = new FontLoader();
-  // const font = loader.parse(fontsFile);
-  loader.load(
+   const loader = new FontLoader();
+   loader.load(
     "https://s3-us-west-2.amazonaws.com/s.cdpn.io/254249/helvetiker_regular.typeface.json",
     function (font) {
-      const geometry = new TextGeometry("My School", {
+      const geometry = new TextGeometry("School #", {
         font: font,
         size: 10,
         height: 1,
         curveSegments: 20,
         // bevelEnabled: true,
-        bevelThickness: 5,
-        // bevelSize: 8,
+        bevelThickness: 0.03,
+        // bevelSize: 0.02,
         // bevelOffset: 0,
         // bevelSegments: 5,
       });
